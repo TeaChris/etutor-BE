6ecfb2b8e77a6133c4ad2c6d42f0cc4ef4b6c7fd
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-08 08:09:00 +0100

    feat: Create authentication controller with initial account creation method

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+/*
+ * ############################################################################### *
+ * Created Date: Sa Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sat Mar 08 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import { catchAsync } from '../../middlewares'
+import { Request, Response } from 'express'
+
+export const createAccount = catchAsync(
+  async (req: Request, res: Response) => {}
+)
