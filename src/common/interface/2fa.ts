049bfd57746fe4022631837854209cd952b234c6
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-08 08:58:55 +0100

    feat: Define two-factor authentication interface

---
+++ src/common/interface/2fa.ts
@@ -0,0 +1,23 @@
+/*
+ * ############################################################################### *
+ * Created Date: Sa Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sat Mar 08 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import { twoFactorTypeEnum } from '@/common/constants'
+
+export interface ITwoFactor {
+  type?: twoFactorTypeEnum
+  secret?: string
+  recoveryCode?: string
+  active: boolean
+  verificationTime?: Date
+  isVerified?: boolean
+}
