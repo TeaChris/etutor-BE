fa18689856133d07fb7c0aebfcb3c96ffb8205ec
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-08 08:09:07 +0100

    feat: Export authentication controller index

---
+++ src/controllers/auth/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Sa Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sat Mar 08 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './auth.controller'
