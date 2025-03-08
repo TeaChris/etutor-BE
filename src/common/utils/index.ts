acc9634a0be0a54b8e4258b99359c43198073486
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-08 10:15:51 +0100

    feat: Export utility functions from common utils module

---
+++ src/common/utils/index.ts
@@ -0,0 +1,15 @@
+/*
+ * ############################################################################### *
+ * Created Date: Sa Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sat Mar 08 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+export * from './app.error'
+export * from './helper'
