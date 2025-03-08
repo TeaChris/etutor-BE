11824cab98e02736c5da1eaa2b0a9a0797f72332
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-08 11:39:32 +0100

    feat: Add centralized error handling middleware for improved error management

---
+++ src/middlewares/error.handler.ts
@@ -0,0 +1,46 @@
+/*
+ * ############################################################################### *
+ * Created Date: Sa Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sat Mar 08 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+import { NextFunction, Request, Response } from 'express'
+import { AppError } from '../common'
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err)
+
+  if (!(err instanceof AppError)) {
+    err = new AppError(
+      err.message || 'Internal Server Error',
+      err.statusCode || 500
+    )
+  }
+
+  const { statusCode, status, message, data, stack } = err
+
+  const errorResponse: any = {
+    status,
+    message,
+    ...(data && { data }),
+  }
+
+  // Include stack trace in development environment
+  if (process.env.NODE_ENV === 'development') {
+    errorResponse.stack = stack
+  }
+
+  res.status(statusCode).json(errorResponse)
+}
