4a8150210ae2b99572a914b9047c4f39768a12dd
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-10 21:55:53 +0100

    feat: Add session controller for handling authenticated user sessions

---
+++ src/controllers/auth/session.controller.ts
@@ -0,0 +1,31 @@
+/*
+ * ############################################################################### *
+ * Created Date: Mo Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Mon Mar 10 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+import { AppError, AppResponse, toJSON } from '../../common'
+import { catchAsync } from '../../middlewares'
+import { Request, Response } from 'express'
+
+export const session = catchAsync(async (req: Request, res: Response) => {
+  const currentUser = req.user
+  if (!currentUser) {
+    throw new AppError('Unauthenticated', 401)
+  }
+
+  const initialData = await currentUser._id
+  return AppResponse(
+    res,
+    200,
+    { ...initialData, user: toJSON(currentUser) },
+    'Authenticated'
+  )
+})
