eed302ffb20535fba37fffa93a13e5f4afcb4dcf
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-11 14:49:40 +0100

    feat: add IHashData interface for handling hash data structures

---
+++ src/common/interface/helper.ts
@@ -0,0 +1,17 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Mar 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+export interface IHashData {
+  id?: string
+  token?: string
+}
