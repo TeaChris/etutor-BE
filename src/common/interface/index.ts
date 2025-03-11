918504dbc18963f89ebe611755845b42c2d68490
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-11 14:03:56 +0100

    chore: export session controller

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Sa Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Sat Mar 08 2025                                              *
+ * Last Modified: Tue Mar 11 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -12,3 +12,4 @@
  */
 export * from './2fa'
 export * from './user'
+export * from './request'
