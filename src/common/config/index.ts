33aca6e3da0e894ede269a62a61fedcf04f9df96
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-11 14:49:27 +0100

    feat: create configuration index file to export environment settings

---
+++ src/common/config/index.ts
@@ -0,0 +1,14 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Mar 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+export * from './environment'
