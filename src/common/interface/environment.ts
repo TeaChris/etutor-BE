a55266a5be5be581f72a5bb4f321c95e9b5c2a27
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-11 14:49:33 +0100

    feat: add IEnvironment interface for application configuration

---
+++ src/common/interface/environment.ts
@@ -0,0 +1,59 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Mar 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+export interface IEnvironment {
+  APP: {
+    NAME?: string
+    PORT: number
+    ENV?: string
+    CLIENT: string
+  }
+  DB: {
+    URL: string
+  }
+  REDIS: {
+    URL: string
+    PORT: number
+    PASSWORD: string
+  }
+  CACHE_REDIS: {
+    URL: string
+  }
+  EMAIL: {
+    API_KEY: string
+  }
+  JWT: {
+    ACCESS_KEY: string
+    REFRESH_KEY: string
+  }
+  JWT_EXPIRES_IN: {
+    ACCESS: string
+    REFRESH: string
+  }
+  FRONTEND_URL: string
+  R2: {
+    ACCESS_KEY_ID: string
+    SECRET_ACCESS_KEY: string
+    REGION: string
+    BUCKET_NAME: string
+    ACCOUNT_ID: string
+    CDN_URL: string
+  }
+  OPENAI: {
+    API_KEY: string
+  }
+  PAYSTACK: {
+    HOST: string
+    SECRET_KEY: string
+  }
+}
