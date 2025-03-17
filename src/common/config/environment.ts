d04685aa61b577f3077cabe93cefb4e67847938f
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-17 18:45:50 +0100

    chore: add a blank line for improved readability in environment configuration

--- src/common/config/environment.ts
+++ src/common/config/environment.ts
@@ -11,6 +11,7 @@
  * ############################################################################### *
  */
 
+
 import type { IEnvironment } from '../interface'
 
 export const ENVIRONMENT: IEnvironment = {
