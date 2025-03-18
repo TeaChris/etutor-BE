9af859bd9f611fe5cce298b06e0c58755d938a45
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-18 14:43:49 +0100

    feat: create index file for queue handlers and templates

---
+++ src/queues/index.ts
@@ -0,0 +1,14 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Mar 18 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './handlers'
+export * from './templates'
