d121c6fa18d770dbedb29dda32ff7c5bdbe0e4d8
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-18 14:12:59 +0100

    feat: create index file for queue handlers and export email handler

---
+++ src/queues/handlers/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Tu Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Tue Mar 18 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './email.handler'
