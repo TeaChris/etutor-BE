a4a8948cdaace8b344e8d275dc5448ca07a3e7a0
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-18 14:12:47 +0100

    feat: export email queue and location interfaces from common interface index

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -5,7 +5,7 @@ import e from 'express'
  * Created Date: Sa Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Tue Mar 11 2025                                              *
+ * Last Modified: Tue Mar 18 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -17,3 +17,5 @@ export * from './user'
 export * from './request'
 export * from './environment'
 export * from './helper'
+export * from './email.queue'
+export * from './location'
