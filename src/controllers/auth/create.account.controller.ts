653a203fecb6bf8a94b38a1c4a1fc39a2a94f767
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-19 06:44:26 +0100

    fix: update createAccount controller to remove unused code and improve clarity

--- src/controllers/auth/create.account.controller.ts
+++ src/controllers/auth/create.account.controller.ts
@@ -3,7 +3,7 @@
  * Created Date: Sa Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Tue Mar 18 2025                                              *
+ * Last Modified: Wed Mar 19 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -21,8 +21,6 @@ import {
   AppError,
   AppResponse,
   hashPassword,
-  generateTokenAndSetCookie,
-  generateVerificationCode,
   setCache,
   sendVerificationEmail,
 } from '../../common'
@@ -51,25 +49,7 @@ export const createAccount = catchAsync(async (req: Request, res: Response) => {
     throw new AppError('User with this username already exists', 409)
   }
 
-  // const existingUser = await UserModel.findOne({
-  //   $or: [{ email }, { username }],
-  // })
-
-  // if (existingUser) {
-  //   let errorMessage = 'User already exists'
-
-  //   if (existingUser.email === email && existingUser.username === username) {
-  //     errorMessage = 'User with this email and username already exists'
-  //   } else if (existingUser.email === email) {
-  //     errorMessage = 'User with this email already exists'
-  //   } else if (existingUser.username === username) {
-  //     errorMessage = 'User with this username already exists'
-  //   }
-  //   throw new AppError(errorMessage, 409)
-  // }
-
   const hashedPassword = await hashPassword(password)
-  // const verificationToken = generateVerificationCode()
 
   const user = await UserModel.create({
     email,
@@ -79,20 +59,14 @@ export const createAccount = catchAsync(async (req: Request, res: Response) => {
     password: hashedPassword,
     provider: Provider.Local,
     isTermAndConditionAccepted,
-    // verificationToken,
-    // verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
   })
 
   if (Object.keys(req.body).length === 0) {
     throw new AppError('Request body is empty', 400)
   }
 
-  await sendVerificationEmail(user, req)
-
-  // await user.save()
-
-  // generateTokenAndSetCookie(res, user._id)
-
-  await setCache(user._id.toString(), toJSON(user, ['password']))
   AppResponse(res, 201, toJSON(user), 'Account created successfully')
+
+  await sendVerificationEmail(user, req)
+  await setCache(user._id.toString(), toJSON(user, ['password']))
 })
