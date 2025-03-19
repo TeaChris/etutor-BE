e6395d98c574be6868f6a1f2ea8edf510e9f2360
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-19 16:07:34 +0100

    feat: export sign-in controller from auth index

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Sa Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Mon Mar 10 2025                                              *
+ * Last Modified: Wed Mar 19 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -13,3 +13,4 @@
 export * from './create.account.controller'
 export * from './session.controller'
 export * from './verify-email'
+export * from './sign.in'
