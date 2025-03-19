dfde2b569cc5adfd5489ffabc4c2561cce095904
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-19 06:44:21 +0100

    feat: add functions to start and stop email queues in index.ts

--- src/queues/index.ts
+++ src/queues/index.ts
@@ -3,12 +3,25 @@
  * Created Date: Tu Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Tue Mar 18 2025                                              *
+ * Last Modified: Wed Mar 19 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
  * Date      	By	Comments                                                   *
  * ############################################################################### *
  */
+
+import { startEmailQueue, stopEmailQueue } from './templates'
+
+const startAllQueuesAndWorkers = async () => {
+  await startEmailQueue()
+}
+
+const stopAllQueuesAndWorkers = async () => {
+  await stopEmailQueue()
+}
+
 export * from './handlers'
 export * from './templates'
+
+export { startAllQueuesAndWorkers, stopAllQueuesAndWorkers }
