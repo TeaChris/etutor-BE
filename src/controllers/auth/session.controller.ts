3258abc287bef6a23ab21d44b4b6bb350889f4ba
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-20 14:49:31 +0100

    fix: update formatting in session controller for improved readability

--- src/controllers/auth/session.controller.ts
+++ src/controllers/auth/session.controller.ts
@@ -3,7 +3,7 @@
  * Created Date: Mo Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Mon Mar 10 2025                                              *
+ * Last Modified: Thu Mar 20 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -17,11 +17,12 @@ import { Request, Response } from 'express'
 
 export const session = catchAsync(async (req: Request, res: Response) => {
   const currentUser = req.user
+
   if (!currentUser) {
     throw new AppError('Unauthenticated', 401)
   }
 
-  const initialData =currentUser._id
+  const initialData = currentUser._id
   return AppResponse(
     res,
     200,
